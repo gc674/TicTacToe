388896133992b2599a5088d66f346fe9bd621a4d
Mihai Victor Negrila <negrila.mihai@gmail.com>
2021-02-24 10:08:10 +0200

    Small improvement

--- TicTacToe/Form1.cs
+++ TicTacToe/Form1.cs
@@ -14,6 +14,7 @@ namespace TicTacToe
     public partial class TicTacToe : Form
     {
         private static int turnCounter = 0;
+        private static List<string> Xor0 = new List<string> { "X", "0" };
         public TicTacToe()
         {
             InitializeComponent();
@@ -24,12 +25,12 @@ namespace TicTacToe
             Button button = sender as Button;
             if (turnCounter == 0 || turnCounter % 2 == 0)
             {
-                button.Text = "X";
+                button.Text = Xor0[0];
                 button.Enabled = false;
             }
             else
             {
-                button.Text = "0";
+                button.Text = Xor0[1];
                 button.Enabled = false;
             }
             CheckWinner();
@@ -67,75 +68,90 @@ namespace TicTacToe
             ChangeButtonStatus(true);
             turnCounter = 0;
         }
-
+        public bool CheckBoxes(Button Button1, Button Button2, Button Button3)
+        {
+            if ((Xor0.Contains(Button1.Text, StringComparer.OrdinalIgnoreCase)) && Button1.Text == Button2.Text && Button2.Text == Button3.Text)
+            {
+                Box1.BackColor = Color.LightBlue;
+                Box2.BackColor = Color.LightBlue;
+                Box3.BackColor = Color.LightBlue;
+                return true;
+            }
+            return false;
+        }
         public bool CheckRows()
         {
-            if((Box1.Text.ToLower() == "x" || Box1.Text == "0") && Box1.Text == Box2.Text && Box2.Text == Box3.Text)
-            {
-                Box1.BackColor = Color.LightBlue;
-                Box2.BackColor = Color.LightBlue;
-                Box3.BackColor = Color.LightBlue;
+
+            if (CheckBoxes(Box1, Box2, Box3) || CheckBoxes(Box4, Box5, Box6) || CheckBoxes(Box7, Box8, Box9))
                 return true;
-            }
-            if ((Box4.Text.ToLower() == "x" || Box4.Text == "0") && Box4.Text == Box5.Text && Box5.Text == Box6.Text)
-            {
-                Box4.BackColor = Color.LightBlue;
-                Box5.BackColor = Color.LightBlue;
-                Box6.BackColor = Color.LightBlue;
-                return true;
-            }
-            if ((Box7.Text.ToLower() == "x" || Box7.Text == "0") && Box7.Text == Box8.Text && Box8.Text == Box9.Text)
-            {
-                Box7.BackColor = Color.LightBlue;
-                Box8.BackColor = Color.LightBlue;
-                Box9.BackColor = Color.LightBlue;
-                return true;
-            }
+            //if((Xor0.Contains(Box1.Text, StringComparer.OrdinalIgnoreCase)) && Box1.Text == Box2.Text && Box2.Text == Box3.Text)
+            //{
+            //    Box1.BackColor = Color.LightBlue;
+            //    Box2.BackColor = Color.LightBlue;
+            //    Box3.BackColor = Color.LightBlue;
+            //    return true;
+            //}
+            //if ((Xor0.Contains(Box4.Text, StringComparer.OrdinalIgnoreCase)) && Box4.Text == Box5.Text && Box5.Text == Box6.Text)
+            //{
+            //    Box4.BackColor = Color.LightBlue;
+            //    Box5.BackColor = Color.LightBlue;
+            //    Box6.BackColor = Color.LightBlue;
+            //    return true;
+            //}
+            //if ((Xor0.Contains(Box7.Text, StringComparer.OrdinalIgnoreCase)) && Box7.Text == Box8.Text && Box8.Text == Box9.Text)
+            //{
+            //    Box7.BackColor = Color.LightBlue;
+            //    Box8.BackColor = Color.LightBlue;
+            //    Box9.BackColor = Color.LightBlue;
+            //    return true;
+            //}
             return false;
         }
-
         public bool CheckLines()
         {
-            if ((Box1.Text.ToLower() == "x" || Box1.Text == "0") && Box1.Text == Box4.Text && Box4.Text == Box7.Text)
-            {
-                Box1.BackColor = Color.LightBlue;
-                Box4.BackColor = Color.LightBlue;
-                Box7.BackColor = Color.LightBlue;
+            if (CheckBoxes(Box1, Box4, Box7) || CheckBoxes(Box2, Box5, Box8) || CheckBoxes(Box3, Box6, Box9))
                 return true;
-            }
-            if ((Box2.Text.ToLower() == "x" || Box2.Text == "0") && Box2.Text == Box5.Text && Box5.Text == Box8.Text)
-            {
-                Box2.BackColor = Color.LightBlue;
-                Box5.BackColor = Color.LightBlue;
-                Box8.BackColor = Color.LightBlue;
-                return true;
-            }
-            if ((Box3.Text.ToLower() == "x" || Box3.Text == "0") && Box3.Text == Box6.Text && Box6.Text == Box9.Text)
-            {
-                Box3.BackColor = Color.LightBlue;
-                Box6.BackColor = Color.LightBlue;
-                Box9.BackColor = Color.LightBlue;
-                return true;
-            }
+            //if ((Xor0.Contains(Box1.Text, StringComparer.OrdinalIgnoreCase)) && Box1.Text == Box4.Text && Box4.Text == Box7.Text)
+            //{
+            //    Box1.BackColor = Color.LightBlue;
+            //    Box4.BackColor = Color.LightBlue;
+            //    Box7.BackColor = Color.LightBlue;
+            //    return true;
+            //}
+            //if ((Xor0.Contains(Box2.Text, StringComparer.OrdinalIgnoreCase)) && Box2.Text == Box5.Text && Box5.Text == Box8.Text)
+            //{
+            //    Box2.BackColor = Color.LightBlue;
+            //    Box5.BackColor = Color.LightBlue;
+            //    Box8.BackColor = Color.LightBlue;
+            //    return true;
+            //}
+            //if ((Xor0.Contains(Box3.Text, StringComparer.OrdinalIgnoreCase)) && Box3.Text == Box6.Text && Box6.Text == Box9.Text)
+            //{
+            //    Box3.BackColor = Color.LightBlue;
+            //    Box6.BackColor = Color.LightBlue;
+            //    Box9.BackColor = Color.LightBlue;
+            //    return true;
+            //}
             return false;
         }
-
         public bool CheckDiagonal()
         {
-            if ((Box1.Text.ToLower() == "x" || Box1.Text == "0") && Box1.Text == Box5.Text && Box5.Text == Box9.Text)
-            {
-                Box1.BackColor = Color.LightBlue;
-                Box5.BackColor = Color.LightBlue;
-                Box9.BackColor = Color.LightBlue;
+            if (CheckBoxes(Box1, Box5, Box9) || CheckBoxes(Box3, Box5, Box7))
                 return true;
-            }
-            if ((Box3.Text.ToLower() == "x" || Box3.Text == "0") && Box3.Text == Box5.Text && Box5.Text == Box7.Text)
-            {
-                Box3.BackColor = Color.LightBlue;
-                Box5.BackColor = Color.LightBlue;
-                Box7.BackColor = Color.LightBlue;
-                return true;
-            }
+            //if ((Xor0.Contains(Box1.Text, StringComparer.OrdinalIgnoreCase)) && Box1.Text == Box5.Text && Box5.Text == Box9.Text)
+            //{
+            //    Box1.BackColor = Color.LightBlue;
+            //    Box5.BackColor = Color.LightBlue;
+            //    Box9.BackColor = Color.LightBlue;
+            //    return true;
+            //}
+            //if ((Xor0.Contains(Box3.Text, StringComparer.OrdinalIgnoreCase)) && Box3.Text == Box5.Text && Box5.Text == Box7.Text)
+            //{
+            //    Box3.BackColor = Color.LightBlue;
+            //    Box5.BackColor = Color.LightBlue;
+            //    Box7.BackColor = Color.LightBlue;
+            //    return true;
+            //}
             return false;
         }
 
