87c1c0708150c0ca14747e00a5974786574518cb
Mihai Victor Negrila <negrila.mihai@gmail.com>
2021-02-24 09:23:27 +0200

    Add project files.

---
+++ TicTacToe/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿
+namespace TicTacToe
+{
+    partial class TicTacToe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Box1 = new System.Windows.Forms.Button();
+            this.Box2 = new System.Windows.Forms.Button();
+            this.Box3 = new System.Windows.Forms.Button();
+            this.Box4 = new System.Windows.Forms.Button();
+            this.Box5 = new System.Windows.Forms.Button();
+            this.Box6 = new System.Windows.Forms.Button();
+            this.Box7 = new System.Windows.Forms.Button();
+            this.Box8 = new System.Windows.Forms.Button();
+            this.Box9 = new System.Windows.Forms.Button();
+            this.NewGame = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Box1
+            // 
+            this.Box1.BackColor = System.Drawing.Color.Gray;
+            this.Box1.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Box1.Location = new System.Drawing.Point(132, 168);
+            this.Box1.Name = "Box1";
+            this.Box1.Size = new System.Drawing.Size(100, 100);
+            this.Box1.TabIndex = 0;
+            this.Box1.UseVisualStyleBackColor = false;
+            this.Box1.Click += new System.EventHandler(this.Box_Click);
+            // 
+            // Box2
+            // 
+            this.Box2.BackColor = System.Drawing.Color.Gray;
+            this.Box2.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Box2.Location = new System.Drawing.Point(238, 168);
+            this.Box2.Name = "Box2";
+            this.Box2.Size = new System.Drawing.Size(100, 100);
+            this.Box2.TabIndex = 1;
+            this.Box2.UseVisualStyleBackColor = false;
+            this.Box2.Click += new System.EventHandler(this.Box_Click);
+            // 
+            // Box3
+            // 
+            this.Box3.BackColor = System.Drawing.Color.Gray;
+            this.Box3.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Box3.Location = new System.Drawing.Point(344, 168);
+            this.Box3.Name = "Box3";
+            this.Box3.Size = new System.Drawing.Size(100, 100);
+            this.Box3.TabIndex = 2;
+            this.Box3.UseVisualStyleBackColor = false;
+            this.Box3.Click += new System.EventHandler(this.Box_Click);
+            // 
+            // Box4
+            // 
+            this.Box4.BackColor = System.Drawing.Color.Gray;
+            this.Box4.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Box4.Location = new System.Drawing.Point(132, 274);
+            this.Box4.Name = "Box4";
+            this.Box4.Size = new System.Drawing.Size(100, 100);
+            this.Box4.TabIndex = 3;
+            this.Box4.UseVisualStyleBackColor = false;
+            this.Box4.Click += new System.EventHandler(this.Box_Click);
+            // 
+            // Box5
+            // 
+            this.Box5.BackColor = System.Drawing.Color.Gray;
+            this.Box5.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Box5.Location = new System.Drawing.Point(238, 274);
+            this.Box5.Name = "Box5";
+            this.Box5.Size = new System.Drawing.Size(100, 100);
+            this.Box5.TabIndex = 4;
+            this.Box5.UseVisualStyleBackColor = false;
+            this.Box5.Click += new System.EventHandler(this.Box_Click);
+            // 
+            // Box6
+            // 
+            this.Box6.BackColor = System.Drawing.Color.Gray;
+            this.Box6.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Box6.Location = new System.Drawing.Point(344, 274);
+            this.Box6.Name = "Box6";
+            this.Box6.Size = new System.Drawing.Size(100, 100);
+            this.Box6.TabIndex = 5;
+            this.Box6.UseVisualStyleBackColor = false;
+            this.Box6.Click += new System.EventHandler(this.Box_Click);
+            // 
+            // Box7
+            // 
+            this.Box7.BackColor = System.Drawing.Color.Gray;
+            this.Box7.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Box7.Location = new System.Drawing.Point(132, 380);
+            this.Box7.Name = "Box7";
+            this.Box7.Size = new System.Drawing.Size(100, 100);
+            this.Box7.TabIndex = 6;
+            this.Box7.UseVisualStyleBackColor = false;
+            this.Box7.Click += new System.EventHandler(this.Box_Click);
+            // 
+            // Box8
+            // 
+            this.Box8.BackColor = System.Drawing.Color.Gray;
+            this.Box8.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Box8.Location = new System.Drawing.Point(238, 380);
+            this.Box8.Name = "Box8";
+            this.Box8.Size = new System.Drawing.Size(100, 100);
+            this.Box8.TabIndex = 7;
+            this.Box8.UseVisualStyleBackColor = false;
+            this.Box8.Click += new System.EventHandler(this.Box_Click);
+            // 
+            // Box9
+            // 
+            this.Box9.BackColor = System.Drawing.Color.Gray;
+            this.Box9.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Box9.Location = new System.Drawing.Point(344, 380);
+            this.Box9.Name = "Box9";
+            this.Box9.Size = new System.Drawing.Size(100, 100);
+            this.Box9.TabIndex = 8;
+            this.Box9.UseVisualStyleBackColor = false;
+            this.Box9.Click += new System.EventHandler(this.Box_Click);
+            // 
+            // NewGame
+            // 
+            this.NewGame.Location = new System.Drawing.Point(132, 29);
+            this.NewGame.Name = "NewGame";
+            this.NewGame.Size = new System.Drawing.Size(120, 65);
+            this.NewGame.TabIndex = 9;
+            this.NewGame.Text = "New Game";
+            this.NewGame.UseVisualStyleBackColor = true;
+            this.NewGame.Click += new System.EventHandler(this.NewGame_Click);
+            // 
+            // TicTacToe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(584, 561);
+            this.Controls.Add(this.NewGame);
+            this.Controls.Add(this.Box9);
+            this.Controls.Add(this.Box8);
+            this.Controls.Add(this.Box7);
+            this.Controls.Add(this.Box6);
+            this.Controls.Add(this.Box5);
+            this.Controls.Add(this.Box4);
+            this.Controls.Add(this.Box3);
+            this.Controls.Add(this.Box2);
+            this.Controls.Add(this.Box1);
+            this.Name = "TicTacToe";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TicTacToe";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Box1;
+        private System.Windows.Forms.Button Box2;
+        private System.Windows.Forms.Button Box3;
+        private System.Windows.Forms.Button Box4;
+        private System.Windows.Forms.Button Box5;
+        private System.Windows.Forms.Button Box6;
+        private System.Windows.Forms.Button Box7;
+        private System.Windows.Forms.Button Box8;
+        private System.Windows.Forms.Button Box9;
+        private System.Windows.Forms.Button NewGame;
+    }
+}
+
---
+++ TicTacToe/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TicTacToe.Properties;
+
+namespace TicTacToe
+{
+    public partial class TicTacToe : Form
+    {
+        private static int turnCounter = 0;
+        public TicTacToe()
+        {
+            InitializeComponent();
+        }
+
+        private void Box_Click(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+            if (turnCounter == 0 || turnCounter % 2 == 0)
+            {
+                button.Text = "X";
+                button.Enabled = false;
+            }
+            else
+            {
+                button.Text = "0";
+                button.Enabled = false;
+            }
+            CheckWinner();
+            
+            turnCounter++;
+        }
+
+        public void CheckWinner()
+        {
+            if(CheckRows() || CheckLines() || CheckDiagonal())
+            {
+                MessageBox.Show("Congrats, you won!");
+                ChangeButtonStatus(false);
+            }
+        }
+        public void ChangeButtonStatus(bool active)
+        {
+
+            foreach (var button in Controls.OfType<Button>())
+            {
+
+                if (button.Name != "NewGame")
+                {
+                    button.Enabled = active;
+                    if (active)
+                    {
+                        button.Text = "";
+                        button.BackColor = Color.Gray;
+                    } 
+                }
+            }
+        }
+        private void NewGame_Click(object sender, EventArgs e)
+        {
+            ChangeButtonStatus(true);
+            turnCounter = 0;
+        }
+
+        public bool CheckRows()
+        {
+            if((Box1.Text.ToLower() == "x" || Box1.Text == "0") && Box1.Text == Box2.Text && Box2.Text == Box3.Text)
+            {
+                Box1.BackColor = Color.LightBlue;
+                Box2.BackColor = Color.LightBlue;
+                Box3.BackColor = Color.LightBlue;
+                return true;
+            }
+            if ((Box4.Text.ToLower() == "x" || Box4.Text == "0") && Box4.Text == Box5.Text && Box5.Text == Box6.Text)
+            {
+                Box4.BackColor = Color.LightBlue;
+                Box5.BackColor = Color.LightBlue;
+                Box6.BackColor = Color.LightBlue;
+                return true;
+            }
+            if ((Box7.Text.ToLower() == "x" || Box7.Text == "0") && Box7.Text == Box8.Text && Box8.Text == Box9.Text)
+            {
+                Box7.BackColor = Color.LightBlue;
+                Box8.BackColor = Color.LightBlue;
+                Box9.BackColor = Color.LightBlue;
+                return true;
+            }
+            return false;
+        }
+
+        public bool CheckLines()
+        {
+            if ((Box1.Text.ToLower() == "x" || Box1.Text == "0") && Box1.Text == Box4.Text && Box4.Text == Box7.Text)
+            {
+                Box1.BackColor = Color.LightBlue;
+                Box4.BackColor = Color.LightBlue;
+                Box7.BackColor = Color.LightBlue;
+                return true;
+            }
+            if ((Box2.Text.ToLower() == "x" || Box2.Text == "0") && Box2.Text == Box5.Text && Box5.Text == Box8.Text)
+            {
+                Box2.BackColor = Color.LightBlue;
+                Box5.BackColor = Color.LightBlue;
+                Box8.BackColor = Color.LightBlue;
+                return true;
+            }
+            if ((Box3.Text.ToLower() == "x" || Box3.Text == "0") && Box3.Text == Box6.Text && Box6.Text == Box9.Text)
+            {
+                Box3.BackColor = Color.LightBlue;
+                Box6.BackColor = Color.LightBlue;
+                Box9.BackColor = Color.LightBlue;
+                return true;
+            }
+            return false;
+        }
+
+        public bool CheckDiagonal()
+        {
+            if ((Box1.Text.ToLower() == "x" || Box1.Text == "0") && Box1.Text == Box5.Text && Box5.Text == Box9.Text)
+            {
+                Box1.BackColor = Color.LightBlue;
+                Box5.BackColor = Color.LightBlue;
+                Box9.BackColor = Color.LightBlue;
+                return true;
+            }
+            if ((Box3.Text.ToLower() == "x" || Box3.Text == "0") && Box3.Text == Box5.Text && Box5.Text == Box7.Text)
+            {
+                Box3.BackColor = Color.LightBlue;
+                Box5.BackColor = Color.LightBlue;
+                Box7.BackColor = Color.LightBlue;
+                return true;
+            }
+            return false;
+        }
+
+
+    }
+}
